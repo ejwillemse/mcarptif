d9092e64d1224dd32c9e9846413fda8922ca0376
ejwillemse <ejwillemse@gmail.com>
2020-07-25 17:49:44 +0800

    empty module added for pickup and return data frame.

---
+++ pickup_return_routing/pickup_return_data_prep.py
@@ -0,0 +1,7 @@
+"""
+The pickup and return problem is a simple one where waste has to be
+offloaded after each collection and the vehicle has to return to the
+collection point. It arises in practice when large collection containers are
+directly loaded onto waste trucks, taken for emptying, and then returned to
+the pick-up location.
+"""
